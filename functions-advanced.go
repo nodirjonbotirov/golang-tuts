a3fe964c6f41c05de1d7b9c99f67738126735051
nodirjonbotirov <unknownnod@gmail.com>
2022-11-06 22:43:32 +0500

    advanced functions

---
+++ functions-advanced.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    var counter int = 1
+
+    func(str string) {
+        fmt.Println("Hi", str, "I'm an anonymous function")
+    }("Ricky")
+
+    funcVar := func(str string) {
+        fmt.Println("Hi", str, "I'm an anonymous function assigned to a variable.")
+    }
+
+    funcVar("Johnny")
+
+    closure := func(str string) {
+        fmt.Println("Hi", str, "I'm a closure.")
+        for i := 1; i < 5; i++ {
+            fmt.Println("Counter incremented to:", counter)
+            counter++
+        }
+    }
+
+    fmt.Println("Counter is", counter, "before calling closure.")
+    closure("Sandy")
+    fmt.Println("Counter is", counter, "after calling closure.")
+}
